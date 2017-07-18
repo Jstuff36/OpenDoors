aa872038cf6cf5ca4dcedeeb09eca7629061ca83
Jstuff36 <mizzou_fan10@hotmail.com>
2017-07-18 10:37:53 -0700

    Created user database migration

--- db/migrate/20170718164140_create_users.rb
+++ db/migrate/20170718164140_create_users.rb
@@ -1,8 +1,22 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
-
+      t.string :firstname, null: false
+      t.string :lastname, null: false
+      t.string :email, null: false, uniqueness: true
+      t.string :password_digest, null: false
+      t.string :session_token, null: false, uniqueness: true
+      t.string :languages, array: true, default: []
+      t.string :city, null: false
+      t.string :country, null: false
+      t.string :occupation
+      t.text :about
+      t.boolean :hosting, default: true
       t.timestamps
     end
+
+    add_index :users, :email, unique: true
+    add_index :users, :session_token, unique: true
+
   end
 end
