74183f289bc0359cc2872e768d1dfc88c1da3d14
Jstuff36 <mizzou_fan10@hotmail.com>
2017-07-18 11:49:47 -0700

    Created and tested user and session endpoints

--- frontend/open_doors.jsx
+++ frontend/open_doors.jsx
@@ -3,7 +3,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 //Components
 
+//Testing
+import * as utils from './util/session_api_util';
+
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
   ReactDOM.render(<h1>Hi</h1>, root);
 });
+
+window.util = utils;
