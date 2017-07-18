9b57e67b517e1da93b741fa12fc653d15aa79ccc
Jstuff36 <mizzou_fan10@hotmail.com>
2017-07-17 22:02:16 -0700

    Created anchor point for react to hook into rails

---
+++ frontend/open_doors.jsx
@@ -0,0 +1,9 @@
+//React
+import React from 'react';
+import ReactDOM from 'react-dom';
+//Components
+
+document.addEventListener('DOMContentLoaded', () => {
+  const root = document.getElementById('root');
+  ReactDOM.render(<h1>Hi</h1>, root);
+});
