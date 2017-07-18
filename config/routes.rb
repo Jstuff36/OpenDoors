90994869bb0f701a72735316738bf6d943c4b6f0
Jstuff36 <mizzou_fan10@hotmail.com>
2017-07-18 11:32:56 -0700

    Fixed bug in application controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: {format: :json} do
     resources :users, only: [:create]
-    resource :sesssion, only: [:create, :destroy]
+    resource :session, only: [:create, :destroy]
   end
 
   root "static_pages#root"
