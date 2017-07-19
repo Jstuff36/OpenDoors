d2f4edbf3b553e619a2d2278d4c366866ec2c4b1
Jstuff36 <mizzou_fan10@hotmail.com>
2017-07-18 21:57:47 -0700

    Added logout button

--- frontend/components/listings/listing_map.jsx
+++ frontend/components/listings/listing_map.jsx
@@ -4,11 +4,18 @@ import { Link, withRouter } from 'react-router-dom';
 class ListingMap extends React.Component {
   constructor(props) {
     super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.props.logout().then( () => this.props.history.push({
+      pathname: `/login`
+    }));
   }
 
   render() {
     return(
-      <h1>Map Listing</h1>
+      <button onClick={this.handleClick}>Logout</button>
     );
   }
 }
--- frontend/components/listings/listing_map_container.js
+++ frontend/components/listings/listing_map_container.js
@@ -1,12 +1,13 @@
 import { connect } from 'react-redux';
 import ListingMap from './listing_map';
+import { logout } from '../../actions/session_actions';
 
 const mapStateToProps = () => ({
 
 });
 
 const mapDispatchToProps = (dispatch) => ({
-
+  logout: () => dispatch(logout())
 });
 
 export default connect(
