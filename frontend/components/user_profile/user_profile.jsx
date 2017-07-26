634eb251c113eeed4e319ab6435f890afb867edb
Jstuff36 <mizzou_fan10@hotmail.com>
2017-07-26 11:54:34 -0700

    Create ability to go to edit profile page

--- frontend/components/user_profile/user_profile.jsx
+++ frontend/components/user_profile/user_profile.jsx
@@ -7,27 +7,38 @@ class UserProfile extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      action: {
+        user_profile: true,
+        user_profile_edit: false,
+      },
       currentUser: this.props.currentUser,
       trips: ""
     };
+    this.handleSwitchDisplay = this.handleSwitchDisplay.bind(this);
     this.seperateHostingsAndUserTrips = this.seperateHostingsAndUserTrips.bind(this);
   }
 
-  
-
-
   componentDidMount() {
     const id = parseInt(this.props.match.params.id);
-
-    if (id !== this.props.currentUser.id) {
-      this.props.history.push(`/user/${this.props.currentUser.id}`);
-    }
 
     this.props.fetchAllTrips(id).then( (resp) => {
       this.setState({
         trips: resp.trips
       });
     });
+  }
+
+  handleSwitchDisplay(action) {
+    return (e) => {
+      let tempObj = {
+        user_profile: false,
+        user_profile_edit: false,
+      };
+      tempObj[action] = true;
+      this.setState({action: tempObj});
+      e.preventDefault();
+
+    };
   }
 
   getImage(imageUrl) {
@@ -59,98 +70,130 @@ render() {
   if (!this.state.trips) {
     return <div></div>;
   } else {
-    const allTrips = this.seperateHostingsAndUserTrips();
-    console.log(allTrips);
-    return(
-        <div>
-          <UserNavBar
-            logout={this.props.logout}/>
-          <div className="user-container">
-            <div className="user-side-container">
-              <div
-                className="user-img"
-                style={this.getImage(picture)}>
-              </div>
-              <div className="user-name">
-                {firstname} {lastname}</div>
-            </div>
-            <div className="user-main-container">
-              <div className="user-options-container">
-                <div>
-                  Upcoming Trips
+    if (this.state.action.user_profile) {
+      const allTrips = this.seperateHostingsAndUserTrips();
+      return(
+          <div>
+            <UserNavBar
+              logout={this.props.logout}/>
+            <div className="user-container">
+              <div className="user-side-container">
+                <div
+                  className="user-img"
+                  style={this.getImage(picture)}>
                 </div>
-                <div>
-                  Edit Profile
-                </div>
+                <div className="user-name">
+                  {firstname} {lastname}</div>
               </div>
-              <div className="user-trips-refs-container">
-                 <div className="upcoming-trips-container">
-                   <div className="trips-refs-heading">
-                     Upcoming Trips
-                   </div>
-                   <ul>
-                     {allTrips[0].map( (trip, idx) => (
-                       <Link to={`/listings/${trip.host_id}`}  key={idx}>
-                         <li className="trip-listings-container">
-                           <div
-                             className="trip-img"
-                             style={this.getImage(trip.host_pic)}>
-                           </div>
-                           <div className="trip-listings-info-container">
-                             <div>
-                               {trip.host_firstname + " " + trip.host_lastname }
+              <div className="user-main-container">
+                <div className="user-options-container">
+                  <div className="user-current-page">
+                    My Profile
+                  </div>
+                  <button
+                    className="user-links"
+                    onClick={this.handleSwitchDisplay("user_profile_edit")}>
+                    Edit Profile
+                  </button>
+                </div>
+                <div className="user-trips-refs-container">
+                   <div className="upcoming-trips-container">
+                     <div className="trips-refs-heading">
+                       Upcoming Trips
+                     </div>
+                     <ul>
+                       {allTrips[0].map( (trip, idx) => (
+                         <Link to={`/listings/${trip.host_id}`}  key={idx}>
+                           <li className="trip-listings-container">
+                             <div
+                               className="trip-img"
+                               style={this.getImage(trip.host_pic)}>
                              </div>
-                             <div>
-                               {trip.dates[0] + "-" + trip.dates[1]}
+                             <div className="trip-listings-info-container">
+                               <div>
+                                 {trip.host_firstname + " " + trip.host_lastname }
+                               </div>
+                               <div>
+                                 {trip.dates[0] + "-" + trip.dates[1]}
+                               </div>
+                               <div>
+                                 {trip.status}
+                               </div>
                              </div>
-                             <div>
-                               {trip.status}
+                           </li>
+                        </Link>
+                       ))}
+                     </ul>
+                     <div className="trips-refs-heading">
+                       Upcoming Hostings
+                     </div>
+                     <ul>
+                       {allTrips[1].map( (trip, idx) => (
+                         <Link to={`/listings/${trip.traveler_id}`}  key={idx}>
+                           <li className="trip-listings-container">
+                             <div
+                               className="trip-img"
+                               style={this.getImage(trip.traveler_pic)}>
                              </div>
-                           </div>
-                         </li>
-                      </Link>
-                     ))}
-                   </ul>
-                   <div className="trips-refs-heading">
-                     Upcoming Hostings
-                   </div>
-                   <ul>
-                     {allTrips[1].map( (trip, idx) => (
-                       <Link to={`/listings/${trip.traveler_id}`}  key={idx}>
-                         <li className="trip-listings-container">
-                           <div
-                             className="trip-img"
-                             style={this.getImage(trip.traveler_pic)}>
-                           </div>
-                           <div className="trip-listings-info-container">
-                             <div>
-                               {trip.traveler_firstname + " " + trip.traveler_lastname }
+                             <div className="trip-listings-info-container">
+                               <div>
+                                 {trip.traveler_firstname + " " + trip.traveler_lastname }
+                               </div>
+                               <div>
+                                 {trip.dates[0] + "-" + trip.dates[1]}
+                               </div>
+                               <div>
+                                 {trip.status}
+                               </div>
                              </div>
-                             <div>
-                               {trip.dates[0] + "-" + trip.dates[1]}
-                             </div>
-                             <div>
-                               {trip.status}
-                             </div>
-                           </div>
-                         </li>
-                      </Link>
-                     ))}
-                   </ul>
-                   <div>
+                           </li>
+                        </Link>
+                       ))}
+                     </ul>
+                     <div>
 
+                     </div>
                    </div>
-                 </div>
-                 <div className="user-references-container">
-                   <div className="trips-refs-heading">
-                     My References
+                   <div className="user-references-container">
+                     <div className="trips-refs-heading">
+                       My References
+                     </div>
                    </div>
-                 </div>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      );
+        );
+      } else {
+        return(
+          <div>
+            <UserNavBar
+              logout={this.props.logout}/>
+            <div className="user-container">
+              <div className="user-side-container">
+                <div
+                  className="user-img"
+                  style={this.getImage(picture)}>
+                </div>
+                <div className="user-name">
+                  {firstname} {lastname}</div>
+              </div>
+              <div className="user-main-container">
+                <div className="user-options-container">
+                  <button
+                    className="user-links"
+                    onClick={this.handleSwitchDisplay("user_profile")}>
+                    My Profile
+                  </button>
+                  <div className="user-current-page">
+                    Edit Profile
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        );
+      }
     }
   }
 }
